7544c9b22db5318ec3b8d4cc6e26f3a838f4f929
ViktorKarpovich <vik.karpovich@gmail.com>
2022-08-08 21:48:50 +0300

    Point 2.1 of Task1 completed(possibility to input massive)

--- src/lesson3/Task1.java
+++ src/lesson3/Task1.java
@@ -7,15 +7,29 @@ package lesson3;
 import java.util.Scanner;
 
 public class Task1 {
-
+    static int n;
     public static void main(String[] args) {
-        int n = makeMassiveUsingKeybord();
+        makeMassiveUsingKeybord();
+        int array[] = new int [n];
+        int el = 0;
         System.out.println("Massive size: " + n);
+        for (int i = 0; i < array.length; i++){
+            el =+ el + 1;
+            System.out.print("Enter element " + el + ": ");
+            array[i] = makeMassiveUsingKeybord();
+        }
+        /*for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+        System.out.println("Inputed massive: ");*/
     }
 
     public static int makeMassiveUsingKeybord(){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         return n;
+    }
+     public static String printmassive(){
+
     }
 }
